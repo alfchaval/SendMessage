5af2a150cc3741f65d682431895847b91c1f0bf6
alfchaval <alfchaval@hotmail.com>
2017-09-29 13:43:12 +0200

    29-9-2017

--- app/src/main/java/com/example/usuario/sendmessage/SendMessageActivity.java
+++ app/src/main/java/com/example/usuario/sendmessage/SendMessageActivity.java
@@ -3,12 +3,16 @@ package com.example.usuario.sendmessage;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.usuario.sendmessage.pojo.Message;
+
 /**
  * Esta clase envía un mensaje a otra.
+ *
  * @author Alfonso Chamorro Valle
  */
 
@@ -16,6 +20,8 @@ public class SendMessageActivity extends AppCompatActivity {
 
     private EditText edtUser, edtMessage;
     private Button btnOK1, btnOK2;
+
+    private static final String TAG = "com.example.sendmessage";
 
     /*
         btnOK1 tiene onClick en el layout, mientras que btnOK2 usa un OnClickListener.
@@ -28,8 +34,8 @@ public class SendMessageActivity extends AppCompatActivity {
 
         edtUser = (EditText) findViewById(R.id.edtUser);
         edtMessage = (EditText) findViewById(R.id.edtMessage);
-        btnOK1 = (Button)findViewById(R.id.btnOK1);
-        btnOK2 = (Button)findViewById(R.id.btnOK2);
+        btnOK1 = (Button) findViewById(R.id.btnOK1);
+        btnOK2 = (Button) findViewById(R.id.btnOK2);
 
         btnOK2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +43,8 @@ public class SendMessageActivity extends AppCompatActivity {
                 MostrarMensaje();
             }
         });
+
+        Log.d(TAG, "SendMessage: OnCreate()");
     }
 
     public void getOnClick(View view) {
@@ -49,10 +57,34 @@ public class SendMessageActivity extends AppCompatActivity {
 
     private void MostrarMensaje() {
         Bundle bundle = new Bundle();
-        bundle.putString("strUser", edtUser.getText().toString());
-        bundle.putString("strMessage", edtMessage.getText().toString());
+        Message message = new Message(edtUser.getText().toString(), edtMessage.getText().toString());
+        bundle.putSerializable("message", message);
         Intent intent = new Intent(SendMessageActivity.this, ViewMessageActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "SendMessage: OnStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "SendMessage: OnResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "SendMessage: OnPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "SendMessage: OnStop()");
     }
 }
--- app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
+++ app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
@@ -2,16 +2,22 @@ package com.example.usuario.sendmessage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.example.usuario.sendmessage.pojo.Message;
 
 /**
  * Esta clase recibe un mensaje y lo muestra en pantalla.
+ *
  * @author Alfonso Chamorro Valle
  */
 
 public class ViewMessageActivity extends AppCompatActivity {
 
-    TextView txvViewUser, txvViewMessage;
+    private TextView txvViewUser, txvViewMessage;
+
+    private static final String TAG = "com.example.sendmessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,36 @@ public class ViewMessageActivity extends AppCompatActivity {
         txvViewMessage = (TextView) findViewById(R.id.txvViewMessage);
 
         EscribirMensaje();
+
+        Log.d(TAG, "ViewMessage: OnCreate()");
     }
 
     private void EscribirMensaje() {
-        txvViewUser.setText(String.format(getResources().getString(R.string.el_usuario_te_ha_mandado_el_siguiente_mensaje), getIntent().getExtras().getString("strUser").trim()));
-        txvViewMessage.setText(getIntent().getExtras().getString("strMessage").trim());
+        txvViewUser.setText(String.format(getResources().getString(R.string.el_usuario_te_ha_mandado_el_siguiente_mensaje), ((Message) getIntent().getExtras().getSerializable("message")).getUser().trim()));
+        txvViewMessage.setText(((Message) getIntent().getExtras().getSerializable("message")).getMessage().trim());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ViewMessage: OnStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ViewMessage: OnResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ViewMessage: OnPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ViewMessage: OnStop()");
     }
 }
